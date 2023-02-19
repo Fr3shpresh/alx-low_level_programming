351bf202c463905d6be01292491af4b84528802c
Aniefiok Precious <preshaniefiok2@gmail.com>
2023-02-19 13:21:01 -0800

    base

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * mai - main function
+ * main - main function
  *
  * Return: always 0
  */
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 0 ; i <= 9 ; i++)
+	for (i = 0 ; i < 10 ; i++)
 		printf("%d", i);
 	return (0);
 }
