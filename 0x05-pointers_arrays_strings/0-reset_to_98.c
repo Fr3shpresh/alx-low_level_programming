9bdd6b1459b733ca1680390a6c9c1978784ed67c
Aniefiok Precious <Preshaniefiok2@gmail.com>
2023-02-28 06:10:21 -0800

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer to int as parameter
+ * @n: number to be updated
+ * Return: nothing on success.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN. H
+#define MAIN. H
+void reset_to_98(int *n);
 void puts2(char *str);
 #endif
