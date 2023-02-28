3d9fa72b185edfab6826c5638d482c62a0384c77
Aniefiok Precious <Preshaniefiok2@gmail.com>
2023-02-28 14:28:43 -0800

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: string to be printed
+ *
+ * Return: always 0
+ */
+void puts_half(char *str)
+{
+	int count = 0, i;
+
+	while (*str)
+	{
+		count++;
+		str++;
+	}
+
+	for (i = 0; i < count; i++)
+		str--;
+
+	i = (count % 2 == 0) ? count / 2 : (count + 1) / 2;
+
+	for (; i < count; i++)
+		putchar(str[i]);
+
+	putchar('\n');
+}
