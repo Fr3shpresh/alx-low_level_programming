b92cde76a45447b4bf75b190015452aa2c818e5a
Aniefiok Precious <Preshaniefiok2@gmail.com>
2023-03-12 06:31:33 -0700

    ststic library

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic order
+ * @c: the cjaracter to be checked
+ * Return: 1 if c is a ;etter, otherwise
+ */
+int _isalpa(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
